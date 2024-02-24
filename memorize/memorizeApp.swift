28ba93a10192c4639a6279774a2915dde946c847
Sakib Bin Alim <sakibbinalim@gmail.com>
2024-02-24 21:15:50 +0600

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Sakib Bin Alim on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
